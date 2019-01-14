963894fb34bb7e4bc36317baae5b6df266ed300b
well111 <yulove6@163.com>
2019-01-14 13:28:18 +0800

    update

--- src/page/CoinFlow.js
+++ src/page/CoinFlow.js
@@ -1,10 +1,52 @@
 import React,{ Component } from 'react';
 import {Card, Table} from 'antd';
-
+import axios from 'axios'
 class CoinFlow extends Component {
+  // 列表请求
+	requestList = ()=>{
+		axios.get('/api/head.json').then((res) => {
+					const result = res.data.data;
+					this.renderMap(result);
+		});
+	}
 	componentDidMount(){
 		this.node.scrollIntoView();
+		this.requestList()
 	}
+	renderMap = (result) => {
+		let list = result.route_list;
+		this.map = new window.BMap.Map("container", {enableMapClick: false});
+		let gps2 = list[list.length - 1].split(',');
+		let cPoint = new window.BMap.Point(gps2[0], gps2[1]);
+
+		this.map.centerAndZoom(cPoint, 11);
+		
+		let bikeList = result.bike_list;
+		
+		bikeList.forEach((item) => {
+				let p = item.split(",");
+				let point = new window.BMap.Point(p[0], p[1]);
+				var bikeMarker = new window.BMap.Marker(point, { icon: 
+					new window.BMap.Symbol(window.BMap_Symbol_SHAPE_POINT, {
+							scale: 2,
+							fillColor: "orange",
+							fillOpacity: 0.8
+					}) 
+				});
+				this.map.addOverlay(bikeMarker);
+		})
+		
+		this.addMapControl();
+};
+
+addMapControl = () => {
+		let map = this.map;
+		var top_right_control = new window.BMap.ScaleControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT});
+		var top_right_navigation = new window.BMap.NavigationControl({anchor: window.BMAP_ANCHOR_TOP_RIGHT});
+		map.addControl(top_right_control);
+		map.addControl(top_right_navigation);
+		map.enableScrollWheelZoom(true);
+};
 	render() {
 		const columns = [{
   		title: 'IFC地址',
@@ -69,9 +111,7 @@ class CoinFlow extends Component {
 		}];
 		return (
 			<div ref={node => this.node = node}>
-				<img className="banner" 
-					src={require('../static/map.png')}
-					alt=""/>
+				<div id="container" style={{height:500}}></div>
 				<div className="card-container">
                 	<Card title="转让列表">
                 	    <Table dataSource={data} columns={columns} 
